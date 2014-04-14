fdaccbc403d19ce15b9c230ac09666cc9a49220a
ri <git@tnu.jp>
2014-04-14 22:20:07 +0900

    add PDFBox library

--- aptest/src/main/java/jp/tnu/aptest/App.java
+++ aptest/src/main/java/jp/tnu/aptest/App.java
@@ -1,5 +1,7 @@
 package jp.tnu.aptest;
 
+import org.apache.pdfbox.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public String readPdfText() {
+    	return "not impl";
     }
 }
