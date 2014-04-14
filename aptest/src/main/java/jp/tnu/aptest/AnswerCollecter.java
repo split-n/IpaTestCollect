ca18e68190b6ce814add9d43967821cea7d5122d
ri <git@tnu.jp>
2014-04-14 22:41:22 +0900

    適当に

---
+++ aptest/src/main/java/jp/tnu/aptest/AnswerCollecter.java
@@ -0,0 +1,34 @@
+package jp.tnu.aptest;
+
+import java.io.*;
+import java.util.HashMap;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class AnswerCollecter {
+	private String asText;
+	public static final String[] VALID_ANSWERS = "ア イ ウ エ".split(" ");
+
+	public AnswerCollecter(InputStream pdfFileStream) throws IOException {
+		PDFParser pdfParser = new PDFParser(pdfFileStream);
+		pdfParser.parse();
+		PDDocument pdf = pdfParser.getPDDocument();
+	    PDFTextStripper stripper = new PDFTextStripper();
+	    StringWriter writer = new StringWriter();
+	    stripper.writeText(pdf, writer);
+	    writer.flush();
+	    this.asText = writer.toString();
+	}
+
+	public HashMap<String, Integer> getAnswersMap() {
+		String[] lines = asText.split("\n");
+		HashMap<String, Integer> result = new HashMap<>();
+		for(String line : lines) {
+			
+		}
+
+		return result;
+	}
+}
