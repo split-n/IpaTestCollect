966ff044e5e75a6d09f7bd0806d77a1630ced981
ri <git@tnu.jp>
2014-04-15 22:25:59 +0900

    rm unused

--- aptest/src/main/java/jp/tnu/aptest/App.java
+++ aptest/src/main/java/jp/tnu/aptest/App.java
@@ -2,10 +2,6 @@ package jp.tnu.aptest;
 
 import java.io.*;
 
-import org.apache.pdfbox.pdfparser.*;
-import org.apache.pdfbox.pdmodel.*;
-import org.apache.pdfbox.util.*;
-
 public class App 
 {
     public static void main( String[] args )
