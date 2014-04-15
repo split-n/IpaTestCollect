32068a65fe0889d0219f6263719e48ebcbcecde0
ri <git@tnu.jp>
2014-04-15 16:33:15 +0900

    use list to store answers data

--- aptest/src/main/java/jp/tnu/aptest/AnswerCollecter.java
+++ aptest/src/main/java/jp/tnu/aptest/AnswerCollecter.java
@@ -9,9 +9,12 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 public class AnswerCollecter {
-	private String asText;
 	public static final String[] VALID_ANSWERS = "ア イ ウ エ".split(" ");
     public static final Pattern ANSWER_FORMAT = Pattern.compile("^問 (?<qnum>\\d+) (?<answer>(ア|イ|ウ|エ))$"); 
+
+	private String asText;
+	private ArrayList<String> asList;
+	private Map<String,Integer> answersCountMap;
 
 	public AnswerCollecter(InputStream pdfFileStream) throws IOException {
 		PDFParser pdfParser = new PDFParser(pdfFileStream);
@@ -22,26 +25,47 @@ public class AnswerCollecter {
 	    stripper.writeText(pdf, writer);
 	    writer.flush();
 	    this.asText = writer.toString();
+
+	    makeAnswersList();
+	    makeAnswersCountMap();
 	}
 
-	public Map<String, Integer> getAnswersCountMap() {
-		HashMap<String, Integer> result = new HashMap<>();
-		for(String key : VALID_ANSWERS) {
-			result.put(key,0);
-		}
-		String[] lines = asText.split("\n");
+	private void makeAnswersList() {
+		ArrayList<String> answers = new ArrayList<>();
+		answers.add(0, null);
 
+		String[] lines = asText.split("\n");
 		for(String line : lines) {
 			line = line.trim();
 			Matcher m = ANSWER_FORMAT.matcher(line);
 			if(m.matches()){
-				String key = m.group("answer");
-				result.put(key, result.get(key) + 1);
+				int qnum = Integer.parseInt(m.group("qnum"));
+				String ans = m.group("answer");
+				answers.add(qnum, ans);
 			}
 		}
-		return result;
+
+		this.asList = answers;
 	}
+
+	private void makeAnswersCountMap() {
+		HashMap<String, Integer> result = new HashMap<>();
+		for(String key : VALID_ANSWERS) {
+			result.put(key,0);
+		}
+
+		for(String answer : this.asList) {
+			if(answer != null)
+				result.put(answer, result.get(answer) + 1);
+		}
 	
+		this.answersCountMap = result;
+	}
+
+	public Map<String, Integer> getAnswersCountMap() {
+		return answersCountMap;
+	}
+
 	public String getAsText() {
 		return asText;
 	}
