ad3898bea3877f8b01fc67f5c1e5a6e76ffb931f
split-n <git@tnu.jp>
2014-04-16 23:39:59 +0900

    wrote answer statistics collector

---
+++ aptest/src/main/java/jp/tnu/aptest/AnswerStatistics.java
@@ -0,0 +1,30 @@
+package jp.tnu.aptest;
+
+import java.util.*;
+
+public class AnswerStatistics {
+	private List<AnswerPaper> answers;
+	public AnswerStatistics() {
+		this.answers = new ArrayList<>();
+	}
+
+	public void addAnswer(AnswerPaper paper) {
+		answers.add(paper);
+	}
+
+	public Map<String, Integer> collectAllAnswersCount(){
+		Map<String, Integer> result = new HashMap<>();
+		for(String ans : AnswerPaper.VALID_ANSWERS) {
+			result.put(ans, 0);
+		}
+
+		for(AnswerPaper paper : answers) {
+			for(Map.Entry<String,Integer> set : paper.getAnswersCountMap().entrySet() ) {
+				String key = set.getKey();
+				result.put(key, result.get(key) + set.getValue());
+			}
+		}
+
+		return result;
+	}
+}
---
+++ aptest/src/test/java/jp/tnu/aptest/AnswerStatisticsTest.java
@@ -0,0 +1,45 @@
+package jp.tnu.aptest;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class AnswerStatisticsTest {
+
+	@Test
+	public void collectAllAnswersCountTest() {
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("ア", 18+19);
+		expected.put("イ", 18+14);
+		expected.put("ウ", 18+21);
+		expected.put("エ", 26+26);
+		
+		AnswerPaper p1 = createInstance("/2013h25a_ap_am_ans.pdf");
+		AnswerPaper p2 = createInstance("/2009h21h_fe_am_ans.pdf");
+		
+		AnswerStatistics stat = new AnswerStatistics();
+		stat.addAnswer(p1);
+		stat.addAnswer(p2);
+		
+		Map<String, Integer> actural = stat.collectAllAnswersCount();
+		assertArrayEquals(expected.entrySet().toArray(),actural.entrySet().toArray());
+	}	
+	
+	public AnswerPaper createInstance(String testTargetFileName) {
+		AnswerPaper coll = null;
+		String pdfPath = getClass().getResource(testTargetFileName).getPath();
+		try(FileInputStream is = new FileInputStream(pdfPath)) {
+			coll = new AnswerPaper(is);
+		}catch(IOException ex) {
+			fail(ex.toString());
+		}
+		
+		return coll;
+	}
+
+}
